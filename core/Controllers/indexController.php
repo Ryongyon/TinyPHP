3511fd86bcea6013e74c681ab412205ad13ef20d
拾叁 <admin@ryongyon.com>
2020-03-27 00:44:49 +0800

    Update indexController.php

--- core/Controllers/indexController.php
+++ core/Controllers/indexController.php
@@ -4,14 +4,14 @@
 **********************/
 namespace Core\Controllers;
 use Core\Controller;
-use Core\Models\indexModel;
+use Core\Models\optionsModel;
 
 class indexController extends Controller {
 
     // 初始化
     public function init() {
         // 操作模型
-        $options = (new indexModel())->getOptions();
+        $options = (new optionsModel())->getAll();
         // 分配变量
         $this->assign('options', $options);
         // 渲染视图
